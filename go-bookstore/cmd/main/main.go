837b72d14a2c2cec116f282c2d97636667a01954
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-07 23:31:24 +0530

    add package main, register routes

---
+++ go-bookstore/cmd/main/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/kartikchauhan/go-bookstore/pkg/routes"
+)
+
+func main() {
+	r := mux.NewRouter()
+	routes.RegisterBookStoreRoutes(r)
+	http.Handle("/", r)
+
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		log.Fatal("Unable to start server")
+	}
+}
