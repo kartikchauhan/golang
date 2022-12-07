f883671fb948f310456b3b5df700a8563b0efc94
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-08 00:49:55 +0530

    hide updateBook function, to be taken in the next phase

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -2,12 +2,13 @@ package routes
 
 import (
 	"github.com/gorilla/mux"
+	"github.com/kartikchauhan/go-bookstore/pkg/controllers"
 )
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/books", controllers.getBooks).Methods("GET")
 	router.HandleFunc("/book/{id}", controllers.getBookById).Methods("GET")
 	router.HandleFunc("/books", controllers.createBook).Methods("POST")
-	router.HandleFunc("/books/{id}", controllers.updateBook).Methods("PUT")
+	// router.HandleFunc("/books/{id}", controllers.updateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", controllers.deleteBook).Methods("DELETE")
 }
