057e34465b2feb3995673cd75b8ff2ef2b2e9368
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-08 00:03:53 +0530

    add methods GetAllBooks, GetBookById

--- go-bookstore/pkg/controllers/bookstore-controller.go
+++ go-bookstore/pkg/controllers/bookstore-controller.go
@@ -1,2 +1,41 @@
 package controllers
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/gorilla/mux"
+	"github.com/kartikchauhan/go-bookstore/pkg/models"
+)
+
+var NewBook models.Book
+
+func GetAllBooks(w http.ResponseWriter, r *http.Request) {
+	newBooks := models.GetAllBooks()
+	res, _ := json.Marshal(newBooks)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func GetBookById(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id := params["id"]
+
+	bookId, err := strconv.ParseInt(id, 0, 0)
+
+	if err != nil {
+		fmt.Println("Error while parsing")
+	}
+
+	book, _ := models.GetBookById(bookId)
+
+	res, _ := json.Marshal(book)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
