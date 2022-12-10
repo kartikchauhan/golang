270550af652b7fd34c6b7783690cb3c95c8d92d8
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-10 22:53:16 +0530

    add function UpdateBook

--- go-bookstore/pkg/controllers/bookstore-controller.go
+++ go-bookstore/pkg/controllers/bookstore-controller.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
 
@@ -62,14 +63,51 @@ func DeleteBook(w http.ResponseWriter, r *http.Request) {
 
 	params := mux.Vars(r)
 	id := params["id"]
+
 	bookId, err := strconv.ParseInt(id, 0, 0)
 	if err != nil {
-		fmt.Println("Error while parsing")
+		log.Println("Error while parsing")
 	}
 
 	book := models.DeleteBook(bookId)
 
 	res, _ := json.Marshal(book)
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	w.Write(res)
+}
+
+func UpdateBook(w http.ResponseWriter, r *http.Request) {
+	utils.PrintMethod("UpdateBook")
+
+	updatedBookDetails := &models.Book{}
+	utils.ParseBody(r, updatedBookDetails)
+
+	params := mux.Vars(r)
+	id := params["id"]
+
+	bookId, err := strconv.ParseInt(id, 0, 0)
+	if err != nil {
+		log.Println("Error while parsing")
+	}
+
+	bookDetails, db := models.GetBookById(bookId)
+
+	if updatedBookDetails.Name != "" {
+		bookDetails.Name = updatedBookDetails.Name
+	}
+
+	if updatedBookDetails.Author != "" {
+		bookDetails.Author = updatedBookDetails.Author
+	}
+
+	if updatedBookDetails.Publication != "" {
+		bookDetails.Publication = updatedBookDetails.Publication
+	}
+
+	db.Save(&bookDetails)
+
+	res, _ := json.Marshal(bookDetails)
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
 	w.Write(res)
