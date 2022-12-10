3674844f91c44d23ed8ab207e5782f2762c550a8
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-10 22:28:17 +0530

    add logs

--- go-bookstore/cmd/main/main.go
+++ go-bookstore/cmd/main/main.go
@@ -14,6 +14,10 @@ func main() {
 	routes.RegisterBookStoreRoutes(r)
 	http.Handle("/", r)
 
+	log.Println("Routes registered")
+
+	log.Println("Starting server on port 8080")
+
 	if err := http.ListenAndServe(":8080", r); err != nil {
 		log.Fatal("Unable to start server")
 	}
