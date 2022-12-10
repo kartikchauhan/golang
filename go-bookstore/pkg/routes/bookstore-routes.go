95a6a68f2c54768a3f394b0b01619a720e8aa54e
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-10 22:29:38 +0530

    GET /book/{id} -> /books/{id}

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -7,7 +7,7 @@ import (
 
 var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
-	router.HandleFunc("/book/{id}", controllers.GetBookById).Methods("GET")
+	router.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
 	// router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
