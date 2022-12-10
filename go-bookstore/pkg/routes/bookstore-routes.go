242f12a31bc914f2d646744a7df7a689c12b369c
kartikchauhan <chauhan.kartik25@gmail.com>
2022-12-10 22:53:42 +0530

    add update book route

--- go-bookstore/pkg/routes/bookstore-routes.go
+++ go-bookstore/pkg/routes/bookstore-routes.go
@@ -9,6 +9,6 @@ var RegisterBookStoreRoutes = func(router *mux.Router) {
 	router.HandleFunc("/books", controllers.GetBooks).Methods("GET")
 	router.HandleFunc("/books/{id}", controllers.GetBookById).Methods("GET")
 	router.HandleFunc("/books", controllers.CreateBook).Methods("POST")
-	// router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
+	router.HandleFunc("/books/{id}", controllers.UpdateBook).Methods("PUT")
 	router.HandleFunc("/books/{id}", controllers.DeleteBook).Methods("DELETE")
 }
